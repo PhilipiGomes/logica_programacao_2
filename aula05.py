0a8737a7981f09d7669442c067ffcc1c0e5660ce
PhilipiGomes <philipi.rodrigues342@gmail.com>
2024-08-27 09:11:29 -0300

    aula05

---
+++ aula05.py
@@ -0,0 +1,10 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+print(lista)            # imprime a lista toda
+print(lista[0])         # imprime o elemento de indice 0
+print(lista[3:8])       # imprime do indice 3 ao indice 7
+print(lista[:4])        # imprime os 4 primeiros elementos da lista
+print(lista[3:])        # imprime os do indide 3 até o último
+print(lista.index(4))   # imprime o indice do elemento 4
+print(lista[-1])        # imprime o último elemento da lista
+print(lista[-3:])       # imprime os últimos 3 elementos da lista
